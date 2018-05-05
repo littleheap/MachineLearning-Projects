b117cd2abb9d85c4b8c69ba28cde1210c62bbebe
LittleHeap <418761605@qq.com>
2018-05-06 00:33:21 +0800

    update

--- 2.竞争性关键字推荐算法/final/test.py
+++ 2.竞争性关键字推荐算法/final/test.py
@@ -1,6 +1,16 @@
-import re
+# import re
+#
+# print(len('22DD920316420BE2DF8D6EE651BA174B'))  # >>> 32
+#
+# word = re.sub("[A-Za-z0-9\!\%\[\]\,\。]", "", '奥迪')
+# print(word)
 
-print(len('22DD920316420BE2DF8D6EE651BA174B'))  # >>> 32
+# print(type(0.0026455339490908817))
 
-word = re.sub("[A-Za-z0-9\!\%\[\]\,\。]", "", '奥迪')
-print(word)
+dic = {'a': 1, 'b': 2, 'c': 4, 'd': 3}
+dic = sorted(dic.items(), key=lambda d: d[1], reverse=False)
+print(dic)
+print(type(dic))
+dic = dict(dic)
+print(dic)
+print(type(dic))

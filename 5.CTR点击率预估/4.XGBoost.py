56cef6593fa6c4d2fda63901ed12cfed3ad19f51
LittleHeap <418761605@qq.com>
2018-08-29 22:45:48 +0800

    update

--- 5.CTR点击率预估/4.XGBoost.py
+++ 5.CTR点击率预估/4.XGBoost.py
@@ -1,3 +1,7 @@
+import os
+import xgboost as xgb
+import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import Binarizer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV
@@ -6,10 +10,16 @@ from sklearn.model_selection import cross_val_score, train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import xgboost as xgb
 from tools import *
+import warnings
+
+os.environ["OMP_NUM_THREADS"] = "8"  # 并行训练
+rng = np.random.RandomState(4315)
+warnings.filterwarnings("ignore")
 
 '''
-    特征工程+随机森林建模
+    特征工程+XGBoost
 '''
 
 ############################################################################
@@ -25,7 +35,7 @@ train_data = read_csv_file('./data/train.csv', logging=True)
     2      0     170000             NaN        4465  1857485        7434   
     3      0     170000             NaN        1004  2038823         977   
     4      0     170000             NaN        1887  2015141        3688   
-    
+
        connectionType  telecomsOperator  
     0               1                 1  
     1               1                 2  
@@ -44,7 +54,7 @@ train_data = read_csv_file('./data/train.csv', logging=True)
     50%    0.000000e+00  2.418390e+05   242106.000000  3.465000e+03  1.407062e+06   
     75%    0.000000e+00  2.722170e+05   272344.000000  4.565000e+03  2.105989e+06   
     max    1.000000e+00  3.023590e+05   302359.000000  6.582000e+03  2.805118e+06   
-    
+
              positionID  connectionType  telecomsOperator  
     count  3.749528e+06    3.749528e+06      3.749528e+06  
     mean   3.702799e+03    1.222590e+00      1.605879e+00  
@@ -85,7 +95,7 @@ ad = read_csv_file('./data/ad.csv', logging=True)
     50%    3291.500000  1771.000000   274.000000     54.000000   389.000000   
     75%    4936.750000  2698.750000   512.000000     57.000000   421.000000   
     max    6582.000000  3616.000000   720.000000     91.000000   472.000000   
-    
+
            appPlatform  
     count  6582.000000  
     mean      1.448952  
@@ -141,7 +151,7 @@ user = read_csv_file('./data/user.csv', logging=True)
     2       3    0       2          4               0         0         0   
     3       4   21       2          5               3         0       607   
     4       5   22       2          0               0         0         0   
-    
+
        residence  
     0        503  
     1       1403  
@@ -160,7 +170,7 @@ user = read_csv_file('./data/user.csv', logging=True)
     50%    1.402560e+06  2.000000e+01  1.000000e+00  2.000000e+00    1.000000e+00   
     75%    2.103839e+06  2.700000e+01  2.000000e+00  3.000000e+00    2.000000e+00   
     max    2.805118e+06  8.000000e+01  2.000000e+00  7.000000e+00    3.000000e+00   
-    
+
                haveBaby      hometown     residence  
     count  2.805118e+06  2.805118e+06  2.805118e+06  
     mean   2.848418e-01  6.750372e+02  9.571084e+02  
@@ -201,7 +211,7 @@ test_data = read_csv_file('./data/test.csv', True)
     2           3     -1     310000        1456  2769125        5510   
     3           4     -1     310000        4565     9762        4113   
     4           5     -1     310000          49  2513636        3615   
-    
+
        connectionType  telecomsOperator  
     0               1                 3  
     1               1                 3  
@@ -220,7 +230,7 @@ test_data = read_csv_file('./data/test.csv', True)
     50%    169245.000000      -1.0  311536.000000    3012.000000  1.411134e+06   
     75%    253867.000000      -1.0  311951.000000    4565.000000  2.108981e+06   
     max    338489.000000      -1.0  312359.000000    6580.000000  2.805117e+06   
-    
+
               positionID  connectionType  telecomsOperator  
     count  338489.000000   338489.000000     338489.000000  
     mean     3640.126394        1.139015          1.629028  
@@ -248,7 +258,7 @@ print(train_data.head())
     2      0     170000             NaN        4465  1857485        7434   
     3      0     170000             NaN        1004  2038823         977   
     4      0     170000             NaN        1887  2015141        3688   
-    
+
        connectionType  telecomsOperator  clickTime_day  clickTime_hour  
     0               1                 1             17               0  
     1               1                 2             17               0  
@@ -288,14 +298,14 @@ print(user.head())
     2       3    0       2          4               0         0         0   
     3       4   21       2          5               3         0       607   
     4       5   22       2          0               0         0         0   
-    
+
        residence  age_process  hometown_province  hometown_city  \
     0        503            4                 51              2   
     1       1403            2                 14              3   
     2          0            0                  0              0   
     3        607            2                 60              7   
     4       1301            2                  0              0   
-    
+
        residence_province  residence_city  
     0                  50               3  
     1                  14               3  
@@ -388,161 +398,17 @@ x_user_ad_app = np.array(x_user_ad_app, dtype='int32')
 # 标签部分数据转换
 y_user_ad_app = train_user_ad_app.loc[:, ['label']].values
 
-############################################################################
-############################################################################
-
-# 随机森林建模&&特征重要度排序
-
-# 提取需要的特征
-feat_labels = np.array(
-    ['creativeID', 'userID', 'positionID', 'connectionType', 'telecomsOperator', 'clickTime_day', 'clickTime_hour',
-     'age', 'gender', 'education', 'marriageStatus', 'haveBaby', 'residence', 'age_process', 'hometown_province',
-     'hometown_city', 'residence_province', 'residence_city', 'adID', 'camgaignID', 'advertiserID', 'appID',
-     'appPlatform', 'app_categories_first_class', 'app_categories_second_class'])
-
-# 超参数随机初始
-forest = RandomForestClassifier(n_estimators=100,
-                                random_state=0,
-                                n_jobs=-1)
-
-forest.fit(x_user_ad_app, y_user_ad_app.reshape(y_user_ad_app.shape[0], ))
-
-importances = forest.feature_importances_
-
-indices = np.argsort(importances)[::-1]
-
-print(train_user_ad_app.shape)  # (3749528, 30)
-
-print(importances)
-
-'''
-    array([ 0.02791289,  0.16602297,  0.06583927,  0.00427082,  0.03330029,
-            0.0773544 ,  0.03919645,  0.07549754,  0.01282422,  0.04891296,
-            0.03127753,  0.01864926,  0.09910738,  0.01570717,  0.05421798,
-            0.04832801,  0.06373888,  0.05784857,  0.01900962,  0.01561504,
-            0.00836031,  0.00592996,  0.00088301,  0.00322788,  0.00696759])
-'''
-
-'''
-     1) userID                         0.166023
-     2) residence                      0.099107
-     3) clickTime_day                  0.077354
-     4) age                            0.075498
-     5) positionID                     0.065839
-     6) residence_province             0.063739
-     7) residence_city                 0.057849
-     8) hometown_province              0.054218
-     9) education                      0.048913
-    10) hometown_city                  0.048328
-    11) clickTime_hour                 0.039196
-    12) telecomsOperator               0.033300
-    13) marriageStatus                 0.031278
-    14) creativeID                     0.027913
-    15) adID                           0.019010
-    16) haveBaby                       0.018649
-    17) age_process                    0.015707
-    18) camgaignID                     0.015615
-    19) gender                         0.012824
-    20) advertiserID                   0.008360
-    21) app_categories_second_class    0.006968
-    22) appID                          0.005930
-    23) connectionType                 0.004271
-    24) app_categories_first_class     0.003228
-    25) appPlatform                    0.000883
-'''
-
-# 绘制特征重要性直方图
-for f in range(x_user_ad_app.shape[1]):
-    print("%2d) %-*s %f" % (f + 1, 30, feat_labels[indices[f]], importances[indices[f]]))
-
-plt.title('Feature Importances')
-plt.bar(range(x_user_ad_app.shape[1]), importances[indices], color='lightblue', align='center')
-plt.xticks(range(x_user_ad_app.shape[1]), feat_labels[indices], rotation=90)
-plt.xlim([-1, x_user_ad_app.shape[1]])
-plt.tight_layout()
-plt.savefig('./figures/random_forest.png', dpi=300)
-plt.show()
-
-############################################################################
-############################################################################
-
-# 随机森林调参
 param_grid = {
-    'n_estimators': [100],
-    'n_estimators': [10, 100, 500, 1000],
-    'max_features': [0.6, 0.7, 0.8, 0.9]
+    'max_depth': [3, 4, 5, 7, 9],
+    'n_estimators': [10, 50, 100, 400, 800, 1000, 1200],
+    'learning_rate': [0.1, 0.2, 0.3],
+    'gamma': [0, 0.2],
+    'subsample': [0.8, 1],
+    'colsample_bylevel': [0.8, 1]
 }
 
-rf = RandomForestClassifier()
-rfc = GridSearchCV(rf, param_grid, scoring='neg_log_loss', cv=3, n_jobs=2)
-rfc.fit(x_user_ad_app, y_user_ad_app.reshape(y_user_ad_app.shape[0], ))
-print(rfc.best_score_)
-print(rfc.best_params_)
-
-############################################################################
-############################################################################
-
-# 正负样本比
-positive_num = train_user_ad_app[train_user_ad_app['label'] == 1].values.shape[0]
-
-negative_num = train_user_ad_app[train_user_ad_app['label'] == 0].values.shape[0]
-
-print(negative_num / float(positive_num))  # 39.20424181338595
-
-'''
-    我们可以看到正负样本数量相差非常大，数据严重unbalanced
-    我们用Bagging修正过后，处理不均衡样本的Blagging来进行训练和实验。
-'''
-
-############################################################################
-############################################################################
-
-from blagging import *
-
-# help(BlaggingClassifier)
-
-# 处理unbalanced的classifier
-classifier = BlaggingClassifier(n_jobs=-1)
-
-# 预测
-classifier.predict_proba(x_test_clean)
-
-############################################################################
-############################################################################
-
-# 预测
-test_data = pd.merge(test_data, user, on='userID')
-test_user_ad = pd.merge(test_data, ad, on='creativeID')
-test_user_ad_app = pd.merge(test_user_ad, app_categories, on='appID')
-
-x_test_clean = test_user_ad_app.loc[:, ['creativeID', 'userID', 'positionID',
-                                        'connectionType', 'telecomsOperator', 'clickTime_day', 'clickTime_hour', 'age',
-                                        'gender', 'education',
-                                        'marriageStatus', 'haveBaby', 'residence', 'age_process',
-                                        'hometown_province', 'hometown_city', 'residence_province', 'residence_city',
-                                        'adID', 'camgaignID', 'advertiserID', 'appID', 'appPlatform',
-                                        'app_categories_first_class', 'app_categories_second_class']].values
-
-x_test_clean = np.array(x_test_clean, dtype='int32')
-
-result_predict_prob = []
-result_predict = []
-for i in range(scale):
-    result_indiv = clfs[i].predict(x_test_clean)
-    result_indiv_proba = clfs[i].predict_proba(x_test_clean)[:, 1]
-    result_predict.append(result_indiv)
-    result_predict_prob.append(result_indiv_proba)
-
-result_predict_prob = np.reshape(result_predict_prob, [-1, scale])
-result_predict = np.reshape(result_predict, [-1, scale])
-
-result_predict_prob = np.mean(result_predict_prob, axis=1)
-result_predict = max_count(result_predict)
-
-result_predict_prob = np.array(result_predict_prob).reshape([-1, 1])
-
-test_data['prob'] = result_predict_prob
-test_data = test_data.loc[:, ['instanceID', 'prob']]
-test_data.to_csv('predict.csv', index=False)
-print("prediction done!")
-
+xgb_model = xgb.XGBClassifier()
+rgs = GridSearchCV(xgb_model, param_grid, n_jobs=-1)
+rgs.fit(x_user_ad_app, y_user_ad_app.reshape(y_user_ad_app.shape[0], ))
+print(rgs.best_score_)
+print(rgs.best_params_)
